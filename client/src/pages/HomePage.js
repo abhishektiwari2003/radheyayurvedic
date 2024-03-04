6b69a6873f57c4f9e70c0bfbd5d84aa76ab4830d
Abhishek Tiwari <tiwari.abhishek2021@vitbhopal.ac.in>
2024-03-04 16:06:51 +0530

    Mui css update

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -2,12 +2,23 @@ import React, { useState, useEffect } from "react";
 import Layout from "./../components/Layout/Layout";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { Checkbox, Radio } from "antd";
 import { Prices } from "../components/Prices";
 import { useCart } from "../context/cart";
 import toast from "react-hot-toast";
 import "../styles/Homepage.css";
 import { AiOutlineReload } from "react-icons/ai";
+import {
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
+  RadioGroup,
+  Radio,
+  FormControlLabel,
+  Stack,
+  Typography,
+} from "@mui/material";
 
 const HomePage = () => {
   const [products, setProducts] = useState([]);
@@ -120,16 +131,19 @@ const HomePage = () => {
   return (
     <Layout title={"ALl Products - Best offers "}>
       {/* banner image */}
-      <img
+      {/* <img
         src="/images/banner.png"
         className="banner-img"
         alt="bannerimage"
         width={"100%"}
-      />
+      /> */}
       {/* banner image */}
-      <div className="container-fluid row mt-3 home-page">
-        <div className="col-md-3 filters">
-          <h4 className="text-center">Filter By Category</h4>
+      <Grid
+        container
+        sx={{ display: "flex", flexDirection: "row", justifyContent: "center" }}
+      >
+        {/* <div className="col-md-3 filters"> */}
+        {/* <h4 className="text-center">Filter By Category</h4>
           <div className="d-flex flex-column">
             {categories?.map((c) => (
               <Checkbox
@@ -139,9 +153,9 @@ const HomePage = () => {
                 {c.name}
               </Checkbox>
             ))}
-          </div>
-          {/* price filter */}
-          <h4 className="text-center mt-4">Filter By Price</h4>
+          </div> */}
+        {/* price filter */}
+        {/* <h4 className="text-center mt-4">Filter By Price</h4>
           <div className="d-flex flex-column">
             <Radio.Group onChange={(e) => setRadio(e.target.value)}>
               {Prices?.map((p) => (
@@ -150,68 +164,118 @@ const HomePage = () => {
                 </div>
               ))}
             </Radio.Group>
-          </div>
-          <div className="d-flex flex-column mx-1">
+          </div> */}
+        {/* <div className="d-flex flex-column mx-1">
             <button
               className="btn btn-danger"
               onClick={() => window.location.reload()}
             >
               RESET FILTERS
             </button>
-          </div>
-        </div>
-        <div className="col-md-9 ">
-          <h1 className="text-center display-flex">All Products</h1>
-          <div className="d-flex flex-wrap">
+          </div> */}
+        {/* </div> */}
+        <Grid
+          container
+          spacing={2}
+          sm={6}
+          md={8}
+          lg={10}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "row",
+          }}
+          margin={3}
+        >
+          <Stack direction="column" spacing={1} mb={4}>
+            <Typography variant="h4" textAlign={"center"}>
+              India's biggest online marketplace for all ayurvedic medicines
+            </Typography>
+            <Typography variant="body1" textAlign={"center"} color={"green"}>
+              <i> Easy to find product following category </i>
+            </Typography>
+            <Grid item>
+              <RadioGroup
+                value={radio}
+                onChange={(e) => setRadio(e.target.value)}
+                name="category"
+                row
+              >
+                {categories.map((category) => (
+                  <FormControlLabel
+                    key={category._id}
+                    value={category.name}
+                    control={<Radio />}
+                    label={category.name}
+                  />
+                ))}
+              </RadioGroup>
+            </Grid>
+            <Typography
+              variant="h4"
+              sx={{
+                textAlign: "center",
+                marginBottom: "2rem",
+              }}
+            >
+              All Products
+            </Typography>
+          </Stack>
+          <Grid container spacing={2}>
             {products?.map((p) => (
-              <div className="card m-2" key={p._id}>
-                <img
-                  src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${p._id}`}
-                  className="card-img-top"
-                  alt={p.name}
-                />
-                <div className="card-body">
-                  <div className="card-name-price">
-                    <h5 className="card-title">{p.name}</h5>
-                    <h5 className="card-title card-price">
+              <Grid item key={p._id} xs={12} sm={6} md={4} lg={3}>
+                <Card height="100%">
+                  <CardMedia
+                    component="img"
+                    height="100%"
+                    image={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${p._id}`}
+                    alt={p.name}
+                  />
+                  <CardContent>
+                    <Typography variant="h5">{p.name}</Typography>
+                    <Typography variant="h6">
                       {p.price.toLocaleString("en-US", {
                         style: "currency",
                         currency: "INR",
                       })}
-                    </h5>
-                  </div>
-                  <p className="card-text ">
-                    {p.description.substring(0, 60)}...
-                  </p>
-                  <div className="card-name-price">
-                    <button
-                      className="btn btn-info ms-1"
-                      onClick={() => navigate(`/product/${p.slug}`)}
-                    >
-                      More Details
-                    </button>
-                    <button
-                      className="btn btn-dark ms-1"
-                      onClick={() => {
-                        setCart([...cart, p]);
-                        localStorage.setItem(
-                          "cart",
-                          JSON.stringify([...cart, p])
-                        );
-                        toast.success("Item Added to cart");
-                      }}
-                    >
-                      ADD TO CART
-                    </button>
-                  </div>
-                </div>
-              </div>
+                    </Typography>
+                    <Typography variant="body">
+                      {p.description.substring(0, 60)}...
+                    </Typography>
+                    <Stack direction="row" spacing={2}>
+                      <Button
+                        variant="outlined"
+                        color="info"
+                        size="small"
+                        onClick={() => navigate(`/product/${p.slug}`)}
+                      >
+                        More Details
+                      </Button>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        size="small"
+                        onClick={() => {
+                          setCart([...cart, p]);
+                          localStorage.setItem(
+                            "cart",
+                            JSON.stringify([...cart, p])
+                          );
+                          toast.success("Item Added to cart");
+                        }}
+                      >
+                        ADD TO CART
+                      </Button>
+                    </Stack>
+                  </CardContent>
+                </Card>
+              </Grid>
             ))}
-          </div>
-          <div className="m-2 p-3">
+          </Grid>
+          <Grid item>
             {products && products.length < total && (
-              <button
-                className="btn loadmore"
+              <Button
+                variant="outlined"
                 onClick={(e) => {
                   e.preventDefault();
                   setPage(page + 1);
@@ -225,11 +289,11 @@ const HomePage = () => {
                     Loadmore <AiOutlineReload />
                   </>
                 )}
-              </button>
+              </Button>
             )}
-          </div>
-        </div>
-      </div>
+          </Grid>
+        </Grid>
+      </Grid>
     </Layout>
   );
 };
